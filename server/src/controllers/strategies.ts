4673486d1e274443454ad9db5c44a86477b6a39d
jmcfadd <justinmcfadd4@gmail.com>
2021-11-28 19:06:58 -0500

    Add Relative Strength strategy

--- server/src/controllers/strategies.ts
+++ server/src/controllers/strategies.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 import { BollingerBandsAnalyzer } from '../strategies/BollBands'
+import { RSIAnalyzer } from '../strategies/RSIAnalyzer';
 
 interface BollBandsRequestBody {
   product_id: string;
@@ -7,11 +8,16 @@ interface BollBandsRequestBody {
   standardDeviation: string;
 }
 
+interface RSIRequestBody {
+  product_id: string;
+  interval: string;
+}
+
 export const startBollBands = async (req: Request, res: Response) => {
   try {
     const { product_id, interval, standardDeviation }: BollBandsRequestBody =
       req.body;
-    BollingerBandsAnalyzer.start(product_id, Number(interval), Number(standardDeviation));
+    BollingerBandsAnalyzer.start(product_id, Number(interval));
     res.status(200).send("Strategy started succsessfully");
   } catch (error) {
     console.log(error);
@@ -23,6 +29,28 @@ export const stopBollBands = async (req: Request, res: Response) => {
   try {
     const product_id: string = req.body.product_id;
     BollingerBandsAnalyzer.stop(product_id);
+    res.status(200).send("Strategy stopped succsessfully");
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Something went wrong. Please try again later.");
+  }
+}
+
+export const startRSI = async (req: Request, res: Response) => {
+  try {
+    const { product_id, interval }: RSIRequestBody = req.body;
+    RSIAnalyzer.start(product_id, Number(interval));
+    res.status(200).send("Strategy started succsessfully");
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Something went wrong. Please try again later.");
+  }
+}
+
+export const stopRSI = async (req: Request, res: Response) => {
+  try {
+    const product_id: string = req.body.product_id;
+    RSIAnalyzer.stop(product_id);
     res.status(200).send("Strategy stopped succsessfully");
   } catch (error) {
     console.log(error);
--- server/src/routes/strategyRoutes.ts
+++ server/src/routes/strategyRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { startBollBands, stopBollBands } from "../controllers/strategies";
+import { startBollBands, startRSI, stopBollBands, stopRSI } from "../controllers/strategies";
 
 const router = Router();
 
 router.post("/boll-bands", startBollBands);
 router.get("/boll-bands", stopBollBands);
 
-
+router.post("/rsi", startRSI);
+router.get("/rsi", stopRSI);
 
 export default router;
