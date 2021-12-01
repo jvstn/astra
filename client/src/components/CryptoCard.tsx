9bfb3cceb0be5d993c8351bc520d58cb2244be6f
jmcfadd <justinmcfadd4@gmail.com>
2021-11-30 22:02:32 -0500

    Add CryptoCard component

---
+++ client/src/components/CryptoCard.tsx
@@ -0,0 +1,22 @@
+import { CardActionArea } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import React, { ReactElement, MouseEventHandler } from 'react';
+interface Props {
+  productName: string;
+  onClick: () => void;
+}
+
+export default function CryptoCard({productName, onClick}: Props): ReactElement {
+  return (
+    <Card sx={{ maxWidth: 150 }} onClick={onClick}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          image={`https://cryptologos.cc/logos/thumbs/${productName}.png?v=014`}
+          alt={productName}
+        />
+      </CardActionArea>
+    </Card>
+  );
+}
