7d4f2413014d968f7399a287c09cf0f62609fff6
jmcfadd <justinmcfadd4@gmail.com>
2021-12-05 04:09:38 -0500

    Add authentication slice

---
+++ client/src/features/Auth/authSlice.ts
@@ -0,0 +1,97 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { AsyncInitialState } from "../../store/hooks";
+import { RootState } from "../../store/store";
+interface AuthState extends AsyncInitialState {
+  isAuthenticated: boolean;
+  token: string;
+  userId: string;
+}
+
+const initialState: AuthState = {
+  isAuthenticated: false,
+  token: "",
+  userId: "",
+  loading: "idle",
+  error: null,
+};
+
+export const createUser = createAsyncThunk<
+  any,
+  { username: string; password: string },
+  { state: RootState }
+>("auth/createUser", async ({ username, password }, { rejectWithValue }) => {
+  try {
+    const response = await axios.post("/user/create", {
+      username,
+      password,
+    });
+    return response.data;
+  } catch (error) {
+    return rejectWithValue(error);
+  }
+});
+
+const login = createAsyncThunk<
+  any,
+  { username: string; password: string },
+  { state: RootState }
+>("auth/login", async ({ username, password }, { rejectWithValue }) => {
+  try {
+    const response = await axios.post("/user/login", {
+      username,
+      password,
+    });
+    return response.data;
+  } catch (error) {
+    return rejectWithValue(error);
+  }
+});
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    logout: (state) => {
+      state.isAuthenticated = false;
+      state.token = "";
+      state.userId = "";
+    },
+  },
+  extraReducers: (builder) => {
+    // createUser
+    builder.addCase(createUser.pending, (state, action) => {
+      state.loading = "pending";
+    });
+    builder.addCase(createUser.fulfilled, (state, action) => {
+      state.isAuthenticated = true;
+      state.token = action.payload.token;
+      state.userId = action.payload.userId;
+      state.loading = "succeeded";
+    });
+    builder.addCase(createUser.rejected, (state, action) => {
+      state.loading = "failed";
+      state.error = action.payload as string;
+    });
+    // login
+    builder.addCase(login.pending, (state, action) => {
+      state.loading = "pending";
+    }
+    );
+    builder.addCase(login.fulfilled, (state, action) => {
+      state.isAuthenticated = true;
+      state.token = action.payload.token;
+      state.userId = action.payload.userId;
+      state.loading = "succeeded";
+    }
+    );
+    builder.addCase(login.rejected, (state, action) => {
+      state.loading = "failed";
+      state.error = action.payload as string;
+    }
+    );
+  },
+});
+
+export const {} = authSlice.actions;
+export default authSlice.reducer;
